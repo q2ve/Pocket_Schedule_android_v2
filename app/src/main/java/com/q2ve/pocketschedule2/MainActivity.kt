69f2163af16630d4db5294ba9a3143674370c5a0
q2ve <qwq2eq@gmail.com>
2021-12-06 22:59:03 +0300

    initial commit

---
+++ app/src/main/java/com/q2ve/pocketschedule2/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.q2ve.pocketschedule2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Denis Shishkin
+ * qwq2eq@gmail.com
+ */
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.main_activity)
+	}
+}

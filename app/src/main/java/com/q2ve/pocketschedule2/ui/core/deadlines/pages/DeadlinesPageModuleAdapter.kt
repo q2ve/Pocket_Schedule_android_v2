d3672664238f53ab625e40766dd538c5e35ee5f8
q2ve <qwq2eq@gmail.com>
2022-04-04 19:07:35 +0300

    *|Deadlines| Changing recycler adapter in deadlines page module to RealmRecyclerViewAdapter. In progress, unfinished.

--- app/src/main/java/com/q2ve/pocketschedule2/ui/core/deadlines/pages/DeadlinesPageModuleAdapter.kt
+++ app/src/main/java/com/q2ve/pocketschedule2/ui/core/deadlines/pages/DeadlinesPageModuleAdapter.kt
@@ -10,7 +10,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.q2ve.pocketschedule2.databinding.DeadlinesItemBinding
+import com.q2ve.pocketschedule2.helpers.ButtonAnimator
 import com.q2ve.pocketschedule2.model.dataclasses.RealmItemDeadline
+import io.realm.RealmRecyclerViewAdapter
+import io.realm.RealmResults
 
 /**
  * Created by Denis Shishkin
@@ -25,8 +28,117 @@ import com.q2ve.pocketschedule2.model.dataclasses.RealmItemDeadline
 class DeadlinesPageModuleAdapter(
 	private val onDeadlineClicked: (RealmItemDeadline) -> Unit,
 	private val onDeadlineCheckboxClicked: (RealmItemDeadline) -> Unit,
+	realmResults: RealmResults<RealmItemDeadline>
+): RealmRecyclerViewAdapter<RealmItemDeadline,DeadlinesPageModuleAdapter.RecyclerItemHolder>(
+	realmResults,
+	true,
+	true
+) {
+	class RecyclerItemHolder(
+		viewBinding: DeadlinesItemBinding
+	): RecyclerView.ViewHolder(viewBinding.root) {
+		var statusNormal: ImageView = viewBinding.coreDeadlinesItemStatusNormal
+		var statusClosed: ImageView = viewBinding.coreDeadlinesItemStatusClosed
+		var statusExpired: ImageView = viewBinding.coreDeadlinesItemStatusExpired
+		var title: TextView = viewBinding.coreDeadlinesItemTitle
+		var subject: TextView = viewBinding.coreDeadlinesItemSubject
+		var tagFile: ImageView = viewBinding.coreDeadlinesItemTagFile
+		var tagDescription: ImageView = viewBinding.coreDeadlinesItemTagDescription
+		var tagLinked: ImageView = viewBinding.coreDeadlinesItemTagLinked
+		var checkbox: ImageView = viewBinding.coreDeadlinesItemCheckbox
+		var checkboxChecked: ImageView = viewBinding.coreDeadlinesItemCheckboxChecked
+	}
+	
+	override fun onCreateViewHolder(container: ViewGroup, viewType: Int): RecyclerItemHolder {
+		val binding = DeadlinesItemBinding.inflate(
+			LayoutInflater.from(container.context),
+			container,
+			false
+		)
+		return RecyclerItemHolder(binding)
+	}
+	
+	override fun onBindViewHolder(holder: RecyclerItemHolder, position: Int) {
+		getItem(position)?.let { deadline ->
+			val currentTime = (System.currentTimeMillis()/1000).toInt()
+			
+			//statusImage
+			if (deadline.isClosed == true) {
+				holder.statusNormal.visibility = View.INVISIBLE
+				holder.statusClosed.visibility = View.VISIBLE
+			} else if (deadline.endDate?: 0 < currentTime) {
+				holder.statusNormal.visibility = View.INVISIBLE
+				holder.statusExpired.visibility = View.VISIBLE
+			}
+			
+			//title
+			holder.title.text = deadline.title
+			
+			//subject
+			holder.subject.text = deadline.subject?.name
+			
+			//tagFile TODO("after functional of files will be added")
+			holder.tagFile.visibility = View.GONE
+			
+			//tagDescription
+			if (deadline.description == null || deadline.description == "") {
+				holder.tagDescription.visibility = View.GONE
+			}
+			
+			//tagLinked TODO("It should be displayed when subject exist. But why? We already have subject as string")
+			holder.tagLinked.visibility = View.GONE
+			
+			//checkbox
+			if (deadline.isExternal == true || deadline.isExternal == null) {
+				holder.checkbox.visibility = View.INVISIBLE
+				holder.checkbox.isClickable = false
+				holder.checkboxChecked.isClickable = false
+			}
+			if (deadline.isClosed == true) {
+				holder.checkbox.visibility = View.INVISIBLE
+				holder.checkboxChecked.visibility = View.VISIBLE
+			}
+			
+			//deadline on click listener
+			ButtonAnimator(holder.itemView).animateWeakPressing()
+			holder.itemView.setOnClickListener {
+				onDeadlineClicked(deadline)
+			}
+			
+			//checkbox on click listener
+			holder.checkbox.setOnClickListener {
+				holder.checkbox.visibility = View.INVISIBLE
+				holder.checkboxChecked.visibility = View.VISIBLE
+				holder.statusNormal.visibility = View.INVISIBLE
+				holder.statusClosed.visibility = View.VISIBLE
+				holder.statusExpired.visibility = View.INVISIBLE
+				Handler().postDelayed({ onDeadlineCheckboxClicked(deadline) }, 250)
+			}
+			
+			//checked checkbox on click listener
+			holder.checkboxChecked.setOnClickListener {
+				holder.checkbox.visibility = View.VISIBLE
+				holder.checkboxChecked.visibility = View.INVISIBLE
+				holder.statusClosed.visibility = View.INVISIBLE
+				if (deadline.endDate?: 0 < currentTime) {
+					holder.statusExpired.visibility = View.VISIBLE
+					holder.statusNormal.visibility = View.INVISIBLE
+				} else {
+					holder.statusExpired.visibility = View.INVISIBLE
+					holder.statusNormal.visibility = View.VISIBLE
+				}
+				Handler().postDelayed({ onDeadlineCheckboxClicked(deadline) }, 250)
+			}
+		}
+		
+	}
+}
+
+class DEPRECATEDDeadlinesPageModuleAdapter(
+	private val onDeadlineClicked: (RealmItemDeadline) -> Unit,
+	private val onDeadlineCheckboxClicked: (RealmItemDeadline) -> Unit,
 	private val deadlines: MutableList<RealmItemDeadline>,
-): RecyclerView.Adapter<DeadlinesPageModuleAdapter.RecyclerItemHolder>() {
+): RecyclerView.Adapter<DEPRECATEDDeadlinesPageModuleAdapter.RecyclerItemHolder>() {
 	
 	class RecyclerItemHolder(
 		viewBinding: DeadlinesItemBinding
